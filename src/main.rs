9151c82e00ed30b7f84700ca3312b73e5b3f1f45
Benoît CORTIER <bcortier@proton.me>
2023-04-27 14:17:58 -0400

    feat: use axum

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,53 @@
+#[macro_use]
+extern crate tracing;
+
+use axum::handler::HandlerWithoutStateExt as _;
+use axum::http::StatusCode;
+use axum::routing::get;
+use axum::Router;
 use std::net::SocketAddr;
-use tower::make::Shared;
 use tower_http::services::ServeDir;
+use tracing_subscriber::layer::SubscriberExt as _;
+use tracing_subscriber::util::SubscriberInitExt as _;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "file_server=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 5002));
 
-    // serve the file in the "assets" directory under `/assets`
-    let service = ServeDir::new("assets");
+    let serve_dir = ServeDir::new("assets").not_found_service(handle_404.into_service());
 
-    hyper::Server::bind(&addr)
-        .serve(Shared::new(service))
+    // Try to create the assets folder for convenience
+    let _ = std::fs::create_dir_all("./assets");
+
+    let app = Router::new()
+        .route("/", get(index))
+        .fallback_service(serve_dir);
+
+    let server = axum::Server::bind(&addr);
+
+    info!("Listening on {addr}");
+
+    server
+        .serve(app.into_make_service())
         .await
         .expect("server error");
+}
+
+async fn index() -> &'static str {
+    r"Hello ＼\ ٩( ᐛ )و /／"
+}
+
+async fn handle_404() -> (StatusCode, &'static str) {
+    (
+        StatusCode::NOT_FOUND,
+        "Ressource nowhere to be found… ( ´•ω•` )ｼｮﾎﾞｰﾝ",
+    )
 }
